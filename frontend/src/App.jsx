489c050b6d2b1edb11b425943e728eb642b2b027
nehang28 <nehangpatel0028@gmail.com>
2025-02-28 19:03:54 +0530

    Add auth pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import './App.css'
+import LoginForm from './pages/Auth/login/login'
+
+function App() {
+
+  return (
+    <>
+    <LoginForm />
+    </>
+  )
+}
+
+export default App
